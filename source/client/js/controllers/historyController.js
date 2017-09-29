424c5cc66728865a8876f21f3e5939ce1fe3c109
mordenius <mordenius@gmail.com>
2017-09-29 18:37:32 +0300

    feature(controllers/historyController) parse url

--- source/client/js/controllers/historyController.js
+++ source/client/js/controllers/historyController.js
@@ -1,4 +1,6 @@
 import { ControllerStateStore } from "redux-store-controller";
+import { words } from "lodash";
+import StructureFolding from "@/structureFolding/index";
 
 class HistoryController extends ControllerStateStore {
 	constructor(options) {
@@ -15,8 +17,28 @@ class HistoryController extends ControllerStateStore {
 		const path = options.history.location.pathname;
 
 		options.stores.viewport.set({
-			viewport: `/${path}`
+			viewport: path
 		});
+
+		HistoryController.secondary(options);
+	}
+
+	static secondary(options) {
+		const path = words(options.history.location.pathname, /[^/ ]+/g);
+
+		switch (path[0]) {
+			case "program-types":
+				break;
+			case "program-structures": {
+				options.stores.programStructureTab.set({
+					tab: path[1],
+					tree: StructureFolding[path[1]]
+				});
+				break;
+			}
+			default:
+				break;
+		}
 	}
 }
 
